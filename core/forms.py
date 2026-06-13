03c2ad9faa6a410fd62179376468440153d1ffa6
Raphael Abreu <raphaeldefariaabreu@gmail.com>
2026-06-13 00:30:17 +0000

    adiciona formulário de cadastro

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class PesquisaClimaForm(forms.Form):
 
     cidade = forms.CharField(
         label='Cidade',
         max_length=100
+    )
+    
+class CadastroForm(forms.Form):
+
+    username = forms.CharField(
+        label='Usuário',
+        max_length=150
+    )
+
+    email = forms.EmailField()
+
+    password = forms.CharField(
+        label='Senha',
+        widget=forms.PasswordInput()
     )
