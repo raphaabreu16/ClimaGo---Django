6598f83462cbeea44655dbc272b0b82057f3c405
adrible <gabriel.adib2007@gmail.com>
2026-06-19 19:47:04 -0300

    Atualizar o urls.py

--- core/urls.py
+++ core/urls.py
@@ -9,12 +9,9 @@ urlpatterns = [
     path('pesquisa-clima/', views.pesquisar_clima, name='pesquisa_clima'),
     path('alertas/', views.alertas, name='alertas'),
     path('config/', views.config, name='config')
-<<<<<<< HEAD
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-=======
     path('alertas/', views.alertas, name='alertas'),
     path('config/', views.config, name='config'),
->>>>>>> 0deb9d4abfffe2852ccbd9232eaa264978aa8506
 ]
