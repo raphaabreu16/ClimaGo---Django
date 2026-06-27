514071b458e819f0dd39087d1ef6e3a0f245a1ae
Marcelo <abalukguatemala@gmail.com>
2026-06-26 22:31:18 -0300

    Import EventoCalendario model in views.py

--- core/views.py
+++ core/views.py
@@ -1,10 +1,10 @@
-from .models import EventoCalendario
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import (authenticate,login,logout)
 from django.contrib.auth.decorators import login_required
 from .forms import (PesquisaClimaForm,CadastroForm,LoginForm)
 from .services.weather_api import get_weather
+from .models import EventoCalendario
 
 
 def home(request):
